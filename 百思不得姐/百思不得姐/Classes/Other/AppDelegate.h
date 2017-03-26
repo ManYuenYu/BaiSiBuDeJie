0d6463973ce45f1b3378d5d6928193aa519ea751
ManYuen <btcs00@qq.com>
2017-03-26 22:25:02 +0800

    项目初始化

---
+++ 百思不得姐/百思不得姐/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by ManYuen on 3/26/17.
+//  Copyright © 2017 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思不得姐/百思不得姐/Classes/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐
+//
+//  Created by ManYuen on 3/26/17.
+//  Copyright © 2017 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
